f506435437327f4599b9cbf5ef23eaeb1461a401
Aka <itsnotaka@Gmail.com>
2022-02-10 16:04:58 -0500

    Packages change

---
+++ templates/default/.eslintrc.js
@@ -0,0 +1,44 @@
+/* cSpell:disable */
+/* spell-checker: disable */
+/* spellchecker: disable */
+
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	extends: ['next', 'next/core-web-vitals', 'plugin:react/recommended', 'xo'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: { jsx: true },
+		ecmaVersion: 12,
+		sourceType: 'module',
+		project: './tsconfig.json',
+	},
+	plugins: ['react', '@typescript-eslint'],
+	ignorePatterns: ['**/*.js'],
+	rules: {
+		// Next.js
+		'@typescript-eslint/triple-slash-reference': 'off',
+		'react/react-in-jsx-scope': 'off',
+
+		// Prettier handles these
+		'@typescript-eslint/comma-dangle': 'off',
+		'react/function-component-definition': 'off',
+		'react/jsx-tag-spacing': 'off',
+		'react/no-unescaped-entities': 'off',
+		'no-mixed-operators': 'off',
+		'operator-linebreak': 'off',
+		'@typescript-eslint/naming-convention': 'off',
+		'quote-props': 'off',
+		'@typescript-eslint/quotes': 'off',
+		'react/jsx-curly-newline': 'off',
+		'@typescript-eslint/indent': 'off',
+		'@typescript-eslint/object-curly-spacing': 'off',
+
+		// Bruh
+		'@typescript-eslint/ban-types': 'off',
+		'object-curly-spacing': 'off',
+	},
+};
--- templates/default/next.config.js
+++ templates/default/next.config.js
@@ -1,7 +1,4 @@
 /** @type {import('templates/default/node_modules/next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  images: {
-    domains: ['avatars.githubusercontent.com'],
-  },
 }

6ff1875458103dfc87670b9d1f53113a4bdd96f6
Daniel <70210356+Itsnotaka@users.noreply.github.com>
2023-03-23 10:07:30 -0400

    [chore] updated type import

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,4 +1,4 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
 import { withIronSessionApiRoute } from "iron-session/next";
 
 import { sessionOptions } from "../../libs/session";
